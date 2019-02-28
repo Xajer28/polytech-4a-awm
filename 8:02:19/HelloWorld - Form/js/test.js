5bd82726da294b622dcc9c95167d20b4d27cda23
Xajer28 <xajer28@yahoo.fr>
2019-02-28 15:45:58 +0100

    submit fonctionne

--- 8:02:19/HelloWorld - Form/js/test.js
+++ 8:02:19/HelloWorld - Form/js/test.js
@@ -3,10 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event){
   console.log("Dom loaded");
 });
 
-function showHello(){
-    alert("Hello World !");
-}
-
 function fctHello(){
     var name = document.getElementById("inName");
     var strToshow = 'Hello World';
@@ -18,8 +14,6 @@ function fctHello(){
     alert(strToshow);
 }
 
-//Mail : manoel.deligny@gmail.com
-//---------------------------------------------------
 //Fonction pour Formulaire
 function raz(form) {
   // clearing inputs
@@ -35,32 +29,96 @@ function raz(form) {
           inputs[i].checked = false;   
       }
   }
+  var sel = document.getElementById('lang');
+  var opt;
+    for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+        opt = sel.options[i];
+        if ( opt.selected === true ) {
+            opt.selected =false;
+            opt.checked = "default";
+        }
+    }
   return false;
 }
 
 
-function surligne(champ, erreur)
-{
+function surligne(champ, erreur){
    if(erreur)
       champ.style.backgroundColor = "#fba";
    else
       champ.style.backgroundColor = "";
 }
 
-function veriftxt(champ)
-{
+function veriftxtnom(){
+  var ok;
+  var champ = document.getElementById("txtNom");
    if(champ.value.length < 2 || champ.value.length > 25)
    {
       surligne(champ, true);
-      return false;
+      ok=false;
    }
    else
    {
       surligne(champ, false);
-      return true;
+      ok= true;
    }
+   return ok;
 }
 
-function submit(){
+function veriftxtprenom(){
+  var ok;
+  var champ = document.getElementById("txtPrenom");
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      ok=false;
+   }
+   else
+   {
+      surligne(champ, false);
+      ok= true;
+   }
+   return ok;
+}
+
+
+function verif(){
+
+  var ok=true;
+
+  //Boutons Radios
+  if(!(document.getElementsByName("sexe").selected)){
+    ok=false;
+    alert("Le choix du sexe n'a pas été sélectionné");
+  }
+
+  //Vérification des champs
+  if(!(veriftxtnom()))
+  {
+    ok = false;
+    alert("Veuillez resaisir le nom");
+  }
+  if (!(veriftxtprenom())){
+    ok=false;
+    alert("Veuillez resaisir le prenom");
+  }
+
+  //Langue
+  if(document.getElementById("lang").value=="default"){
+    ok =false;
+    alert("Veuillez choisir une langue");
+  } 
+
+  //RGPD
+  if(!(document.getElementById("AcceptRGPD").checked)){
+    ok =false;
+    alert("Veuillez cocher la case RGPD");
+  }
+
+  //Vérification Générale
+  if(ok){
+    alert("Félicitations");
+  }
+
 
 }
